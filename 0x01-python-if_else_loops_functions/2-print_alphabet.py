a3758adb7762d2d99782b44109bfc38f36ed276d
Prince Aboagye <aboagyeprince717@gmail.com>
2023-05-09 08:57:25 -0700

    Print the ASCII alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for char in range(ord('a'), ord('z')+1):
-    character = chr(char)
-    print(character, end='')
+for char in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(char), end='')
