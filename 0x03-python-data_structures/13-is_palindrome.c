50d069771b1aeff29ff097288b8b4eabe6319916
Prince Aboagye <aboagyeprince717@gmail.com>
2023-05-15 13:28:03 -0700

    Check if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,77 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list
+ * reverse_linked_list - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: 1 if the list is a palindrome, 0 otherwise
+ * Return: pointer to the first node in the new list
  */
-
-int is_palindrome(listint_t **head)
+void reverse_linked_list(listint_t **head)
 {
-	/* Check if the list is empty */
-	if (*head == NULL)
+	listint_t *previous = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
 	{
-		return (1);
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
 	}
 
-	/* Create a pointer to the middle node of the list */
-	listint_t *middle = *head;
-
-	while (middle->next != NULL && middle->next->next != NULL)
-	{
-		middle = middle->next->next;
-	}
-	/* Reverse the second half of the list */
-	listint_t *prev = NULL;
-	listint_t *curr = middle->next;
-	listint_t *next;
-
-	while (curr != NULL)
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
-
-	/* Compare the first half of the list to the reversed second half */
-	listint_t *first = *head;
-
-	while (first != NULL && prev != NULL)
-	{
-		if (first->n != prev->n)
-		{
-			return (0);
-		}
-		first = first->next;
-		prev = prev->next;
-	}
-	/* The list is a palindrome */
-	return (1);
+	*head = previous;
 }
 
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow_node = *head;
+	listint_t *fast_node = *head;
+	listint_t *temporary_node = *head;
+	listint_t *duplicate_node = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return 1;
+
+	while (1)
+	{
+		fast_node = fast_node->next->next;
+		if (fast_node == NULL)
+		{
+			duplicate_node = slow_node->next;
+			break;
+		}
+		if (fast_node->next == NULL)
+		{
+			duplicate_node = slow_node->next->next;
+			break;
+		}
+		slow_node = slow_node->next;
+	}
+
+	reverse_linked_list(&duplicate_node);
+
+	while (duplicate_node && temporary_node)
+	{
+		if (temporary_node->n == duplicate_node->n)
+		{
+			duplicate_node = duplicate_node->next;
+			temporary_node = temporary_node->next;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	if (duplicate_node == NULL)
+		return 1;
+
+	return 0;
+}
---
+++ 0x03-python-data_structures/alt_13-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the list
+ *
+ * Return: 1 if the list is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(listint_t **head)
+{
+	/* Check if the list is empty */
+	if (*head == NULL)
+	{
+		return (1);
+	}
+
+	/* Create a pointer to the middle node of the list */
+	listint_t *middle = *head;
+
+	while (middle->next != NULL && middle->next->next != NULL)
+	{
+		middle = middle->next->next;
+	}
+	/* Reverse the second half of the list */
+	listint_t *prev = NULL;
+	listint_t *curr = middle->next;
+	listint_t *next;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	/* Compare the first half of the list to the reversed second half */
+	listint_t *first = *head;
+
+	while (first != NULL && prev != NULL)
+	{
+		if (first->n != prev->n)
+		{
+			return (0);
+		}
+		first = first->next;
+		prev = prev->next;
+	}
+	/* The list is a palindrome */
+	return (1);
+}
+
