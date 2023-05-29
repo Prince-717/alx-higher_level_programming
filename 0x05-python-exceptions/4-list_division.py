5634d00af44594bfbafc86f8b4559d2ae2476311
Prince Aboagye <aboagyeprince717@gmail.com>
2023-05-29 08:49:18 -0700

    Divide two integers and print the result

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,19 @@
+#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for i in range(list_length):
+    fresh_list = []
+    for index in range(list_length):
         try:
-            ans = (my_list_1[i] / my_list_2[i])
-        except TypeError:
-            print("wrong type")
-            ans = 0
+            answer = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
             print("division by 0")
-            ans = 0
+            answer = 0
+        except TypeError:
+            print("wrong type")
+            answer = 0
         except IndexError:
             print("out of range")
-            ans = 0
+            answer = 0
         finally:
-            new_list.append(ans)
-    return (new_list)
+            fresh_list.append(answer)
+
+    return (fresh_list)
