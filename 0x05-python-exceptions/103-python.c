70e83707ecd037e729b79d25cbd3e86f03d353d8
Prince Aboagye <aboagyeprince717@gmail.com>
2023-05-29 09:54:14 -0700

    Print info about Python lists, bytes and float objects

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,122 +1,108 @@
-#include <stdio.h>
 #include <Python.h>
 
 /**
- * display_python_bytes - Prints bytes information
+ * python_list_print - Prints basic info about Python lists.
  *
- * @pointer: Python Object
- * Return: no return
+ * @ptr_pyobject: A PyObject list object.
  */
-void display_python_bytes(PyObject *pointer)
+void python_list_print(PyObject *ptr_pyobject)
 {
-	char *ptr_string;
-	long int dimension, index, threshold;
+	Py_ssize_t span, mem_alloc, index;
+	const char *type;
+	PyListObject *list = (PyListObject *)ptr_pyobject;
+	PyVarObject *var = (PyVarObject *)ptr_pyobject;
 
-	setbuf(stdout, NULL);
+	span = var->ob_size;
+	mem_alloc = list->allocated;
 
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(pointer))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		setbuf(stdout, NULL);
-		return;
-	}
+	fflush(stdout);
 
-	dimension = ((PyVarObject *)(pointer))->ob_size;
-	ptr_string = ((PyBytesObject *)pointer)->ob_sval;
-
-	printf("  dimension: %ld\n", dimension);
-	printf("  trying ptr_string: %s\n", ptr_string);
-
-	if (dimension >= 10)
-		threshold = 10;
-	else
-		threshold = dimension + 1;
-
-	printf("  first %ld bytes:", threshold);
-
-	index = 0;
-	while (index < threshold)
-	{
-		if (ptr_string[index] >= 0)
-			printf(" %02x", ptr_string[index]);
-		else
-			printf(" %02x", 256 + ptr_string[index]);
-		index++
-	}
-
-	printf("\n");
-	setbuf(stdout, NULL);
-}
-
-/**
- * display_python_float - Prints float information
- *
- * @pointer: Python Object
- * Return: no return
- */
-void display_python_float(PyObject *pointer)
-{
-	double value;
-	char *ptr_nf;
-
-	setbuf(stdout, NULL);
-	printf("[.] float object info\n");
-
-	if (!PyFloat_Check(pointer))
-	{
-		printf("  [ERROR] Invalid Float Object\n");
-		setbuf(stdout, NULL);
-		return;
-	}
-
-	value = ((PyFloatObject *)(pointer))->ob_fval;
-	ptr_nf = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, Py_DTST_FINITE);
-
-	printf("  value: %s\n", ptr_nf);
-	setbuf(stdout, NULL);
-}
-
-/**
- * display_python_list - Prints ptr_list information
- *
- * @pointer: Python Object
- * Return: no return
- */
-void display_python_list(PyObject *pointer)
-{
-	long int dimension, index;
-	PyListObject *ptr_list;
-	PyObject *ptr_obj;
-
-	setbuf(stdout, NULL);
-	printf("[*] Python ptr_list info\n");
-
-	if (!PyList_Check(pointer))
+	printf("[*] Python list info\n");
+	if (strcmp(ptr_pyobject->ob_type->tp_name, "list") != 0)
 	{
 		printf("  [ERROR] Invalid List Object\n");
-		setbuf(stdout, NULL);
 		return;
 	}
 
-	dimension = ((PyVarObject *)(pointer))->ob_size;
-	ptr_list = (PyListObject *)pointer;
-
-	printf("[*] Size of the Python List = %ld\n", dimension);
-	printf("[*] Allocated = %ld\n", ptr_list->allocated);
+	printf("[*] Size of the Python List = %ld\n", span);
+	printf("[*] Allocated = %ld\n", mem_alloc);
 
 	index = 0;
-	while (index < dimension)
+	while (index < span)
 	{
-		ptr_obj = ptr_list->ob_item[index];
-		printf("Element %ld: %s\n", index, ((ptr_obj)->ob_type)->tp_name);
+		type = list->ob_item[index]->ob_type->tp_name;
+		printf("Element %ld: %s\n", index, type);
+		if (strcmp(type, "bytes") == 0)
+			python_bytes_print(list->ob_item[index]);
+		else if (strcmp(type, "float") == 0)
+			python_float_print(list->ob_item[index]);
 
-		if (PyBytes_Check(ptr_obj))
-			display_python_bytes(ptr_obj);
-		if (PyFloat_Check(ptr_obj))
-			display_python_float(ptr_obj);
-		
 		index++;
 	}
-	setbuf(stdout, NULL);
+}
+
+/**
+ * python_bytes_print - Prints basic info about Python byte objects.
+ * @ptr_pyobject: A PyObject byte object.
+ */
+void python_bytes_print(PyObject *ptr_pyobject)
+{
+	Py_ssize_t span, index;
+	PyBytesObject *bytes = (PyBytesObject *)ptr_pyobject;
+
+	fflush(stdout);
+
+	printf("[.] bytes object info\n");
+	if (strcmp(ptr_pyobject->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	printf("  span: %ld\n", ((PyVarObject *)ptr_pyobject)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	if (((PyVarObject *)ptr_pyobject)->ob_size >= 10)
+		span = 10;
+	else
+		span = ((PyVarObject *)ptr_pyobject)->ob_size + 1;
+
+	printf("  first %ld bytes: ", span);
+
+	index = 0;
+	while (index < span)
+	{
+		printf("%02hhx", bytes->ob_sval[index]);
+		if (index == (span - 1))
+			printf("\n");
+		else
+			printf(" ");
+
+		index++;
+	}
+}
+
+/**
+ * python_float_print - Prints basic info about Python float objects.
+ * @ptr_pyobject: A PyObject float object.
+ */
+void python_float_print(PyObject *ptr_pyobject)
+{
+	char *mem_buff = NULL;
+
+	PyFloatObject *object_float = (PyFloatObject *)ptr_pyobject;
+
+	fflush(stdout);
+
+	printf("[.] float object info\n");
+	if (strcmp(ptr_pyobject->ob_type->tp_name, "float") != 0)
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+
+	mem_buff = PyOS_double_to_string(object_float->ob_fval, 'r', 0,
+			Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", mem_buff);
+	PyMem_Free(mem_buff);
 }
