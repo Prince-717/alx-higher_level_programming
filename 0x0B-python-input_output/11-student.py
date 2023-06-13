cfa56f45925242707ff7cd649a2fdde58c508429
Prince Aboagye <aboagyeprince717@gmail.com>
2023-06-13 10:59:05 -0700

    Write a class Student that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Python module that defines a class Student"""
+
+
+class Student:
+    """Define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {n: getattr(self, n) for n in attrs if hasattr(self, n)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        """
+        for n, m in json.items():
+            setattr(self, n, m)
