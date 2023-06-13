c21c330f59d3fba03e1b6d06d3164da32e9749e6
Prince Aboagye <aboagyeprince717@gmail.com>
2023-06-13 11:12:45 -0700

    Write a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,50 @@
 #!/usr/bin/python3
-""" Module to print status code """
-import sys
+"""Read from standard input and calculate metrics
+"""
 
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
-
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
-
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+def print_stats(d_size, state_code):
+    """Print accumulated metrics
+    """
+    print("File d_size: {}".format(d_size))
+    for opener in sorted(state_code):
+        print("{}: {}".format(opener, state_code[opener]))
 
 
 if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+    import sys
+
+    d_size = 0
+    state_code = {}
+    authentic_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    loop_count = 0
 
     try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
+        for file_line in sys.stdin:
+            if loop_count == 10:
+                print_stats(d_size, state_code)
+                loop_count = 1
+            else:
+                loop_count += 1
+
+            file_line = file_line.split()
 
             try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
+                d_size += int(file_line[-1])
+            except (IndexError, ValueError):
                 pass
-            nlines += 1
+
+            try:
+                if file_line[-2] in authentic_codes:
+                    if state_code.get(file_line[-2], -1) == -1:
+                        state_code[file_line[-2]] = 1
+                    else:
+                        state_code[file_line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(d_size, state_code)
+
     except KeyboardInterrupt:
-        magic.print_info()
+        print_stats(d_size, state_code)
         raise
-    magic.print_info()
